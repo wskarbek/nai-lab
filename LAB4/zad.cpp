5ee0e62c9e4a81b58d22ed8bc8de8b87fe2e87d8
Wojciech Skarbek <woj.skarbek@gmail.com>
2020-11-14 14:44:17 +0100

    Code cleanup

--- LAB4/zad.cpp
+++ LAB4/zad.cpp
@@ -7,73 +7,6 @@
 using namespace std;
 using namespace cv;
 
-bool compareX(Point p1, Point p2) { return p1.x < p2.x; }
-bool compareY(Point p1, Point p2) { return p1.y < p2.y; }
-bool compareDist(pair<Point, Point> p1, pair<Point, Point> p2) { return norm(p1.first - p1.second) < norm(p2.first - p2.second); }
-double _distance(Point p1, Point p2) { return sqrt(pow(p1.x - p2.x, 2)) + pow(p1.y - p2.y, 2); }
-
-bool compareContourAreas (vector<Point> c1, vector<Point> c2) {
-	double i = fabs( contourArea(Mat(c1)));
-	double j = fabs( contourArea(Mat(c2)));
-	return ( i > j );
-}
-
-void orderPoints(vector<Point> inpts, vector<Point> &ordered)
-{
-	sort(inpts.begin(), inpts.end(), compareX);
-	vector<Point> lm(inpts.begin(), inpts.begin()+2);
-	vector<Point> rm(inpts.end()-2, inpts.end());
-
-	sort(lm.begin(), lm.end(), compareY);
-	Point tl(lm[0]);
-	Point bl(lm[1]);
-	vector<pair<Point, Point> > tmp;
-	for(size_t i = 0; i< rm.size(); i++)
-	{
-		tmp.push_back(make_pair(tl, rm[i]));
-	}
-
-	sort(tmp.begin(), tmp.end(), compareDist);
-	Point tr(tmp[0].second);
-	Point br(tmp[1].second);
-
-	ordered.push_back(tl);
-	ordered.push_back(tr);
-	ordered.push_back(br);
-	ordered.push_back(bl);
-}
-
-void fourPointTransform(Mat src, Mat &dst, vector<Point> pts)
-{
-	vector<Point> ordered_pts;
-	orderPoints(pts, ordered_pts);
-
-	double wa = _distance(ordered_pts[2], ordered_pts[3]);
-	double wb = _distance(ordered_pts[1], ordered_pts[0]);
-	double mw = max(wa, wb);
-
-	double ha = _distance(ordered_pts[1], ordered_pts[2]);
-	double hb = _distance(ordered_pts[0], ordered_pts[3]);
-	double mh = max(ha, hb);
-
-	Point2f src_[] =
-	{
-			Point2f(ordered_pts[0].x, ordered_pts[0].y),
-			Point2f(ordered_pts[1].x, ordered_pts[1].y),
-			Point2f(ordered_pts[2].x, ordered_pts[2].y),
-			Point2f(ordered_pts[3].x, ordered_pts[3].y),
-	};
-	Point2f dst_[] =
-	{
-			Point2f(0,0),
-			Point2f(mw - 1, 0),
-			Point2f(mw - 1, mh - 1),
-			Point2f(0, mh - 1)
-	};
-	Mat m = getPerspectiveTransform(src_, dst_);
-	warpPerspective(src, dst, m, Size(mw, mh));
-}
-
 int main() {
     char key;
 
@@ -84,12 +17,15 @@ int main() {
 	do {
 		if(cap.read(frame)) {
             key = (waitKey(1000.0/60.0)&0x0ff);
+			
             cvtColor(frame, gray, COLOR_BGR2GRAY);
             GaussianBlur(gray, gray, Size(5,5), 0);
             Canny(gray, gray, 75, 200);
 
             vector<vector<Point>> contours;
             vector<Vec4i> hierarchy;
+
+			//double ratio = frame.rows / 500.0;
 
             findContours(gray, contours, hierarchy, RETR_LIST, CHAIN_APPROX_SIMPLE);
 			sort(
@@ -100,24 +36,38 @@ int main() {
 			 	}
             );
             vector<vector<Point>> approx(contours.size());
-			size_t i;
+			size_t i, j;
 
 			for(i = 0; i < contours.size(); i++) {
 				double peri = arcLength(contours.at(i), true);
 				approxPolyDP(contours.at(i), approx.at(i), 0.02 * peri, true);
 			}
-			
-			sort(approx.begin(), approx.end(), compareContourAreas);
 
 			for(i = 0; i < approx.size(); i++) {
 				drawContours(frame, approx, i, Scalar(0,0,255), 3);
 				if(approx.at(i).size() == 4) break;
 			}
 
-			if(i < approx.size()) {
-				fourPointTransform(frame, scan, approx.at(i));
-				cvtColor(scan, scan, COLOR_BGR2GRAY, 1);
-				imshow("Scan", scan);
+			vector<vector<Point>> docContour;
+			docContour.push_back(approx.at(i));
+
+			if (docContour.size() > 0) {
+				sort(docContour.begin(), docContour.end(),
+					[](auto &a, auto &b) {
+						return contourArea(a, true) > contourArea(b, true);
+					}
+				);
+
+				Mat dst(Size(300,200), CV_8UC3);
+				vector<Point2f> _src = {{0,0}, {dst.cols, 0}, {dst.cols, dst.rows}, {0, dst.rows}};
+				vector<Point2f> _dst;
+				for (auto p: docContour.at(0)) {
+					_dst.push_back(Point2f(p.x, p.y));
+				}
+				auto wrapMat = getPerspectiveTransform(_dst, _src);
+				warpPerspective(frame, dst, wrapMat, Size(dst.cols, dst.rows));
+				flip(dst, dst, +1);
+				imshow("DST", dst);
 			}
             
             imshow("Edge", frame);
