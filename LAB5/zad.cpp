5968c88f071c4d353567278adf7e11012ed5bc37
Wojciech Skarbek <woj.skarbek@gmail.com>
2020-11-17 00:00:23 +0100

    Working lab5

--- LAB5/zad.cpp
+++ LAB5/zad.cpp
@@ -8,43 +8,82 @@
 using namespace std;
 using namespace cv;
 
-void detect(const Mat& frame, CascadeClassifier& cascade) {
-  Mat frame_gray;
-  cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
-  equalizeHist(frame_gray, frame_gray);
-  
-  std::vector<Rect> detectedRect;
-  cascade.detectMultiScale(frame_gray, detectedRect);
-  for (auto& det : detectedRect)
-    rectangle(frame, det, Scalar(255, 255, 0), 2);
-  imshow("Display", frame);
+vector<Rect> detectedCars;
+
+string gen_random(const int len) {
+		string tmp_s;
+		static const char alphanum[] =
+				"0123456789"
+				"ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+				"abcdefghijklmnopqrstuvwxyz";
+		srand( (unsigned) time(NULL) * getpid());
+				for (int i = 0; i < len; ++i) 
+				tmp_s += alphanum[rand() % (sizeof(alphanum) - 1)];
+		return tmp_s;		
 }
 
+void detect(const Mat& frame, CascadeClassifier& cascade) {
+	Mat frameGray;
+	vector<Rect> detectedCar;
+	cvtColor(frame, frameGray, COLOR_BGR2GRAY);
+	equalizeHist(frameGray, frameGray);
+	cascade.detectMultiScale(frameGray, detectedCar, 1.1, 3, 0, Size(50,50));
+	for (auto& det : detectedCar) {
+		Rect near(-1,-1,0,0);
+		if (detectedCars.size() == 0) detectedCars.push_back(det);
+		for (Rect car : detectedCars) {
+			if(car.x - 20 < det.x && car.y < det.x) {
+				near = car;
+				break;
+			} else
+			if(car.x - 20 < det.x && car.y < det.y) {
+				near = car;
+				break;
+			} else continue;
+		}
+		if (near.x < 0) detectedCars.push_back(det);
+		rectangle(frame, det, Scalar(255, 255, 0), 2);
+	}
+	cout << "Count: " << detectedCars.size() << "\n";
+
+	imshow("Display", frame);
+}
+
+/*void detectAndSave(const Mat& frame, CascadeClassifier& cascade) {
+	Mat frame_gray;
+	cvtColor(frame, frame_gray, COLOR_BGR2GRAY);
+	equalizeHist(frame_gray, frame_gray);
+	
+	int i = 0;
+	std::vector<Rect> detectedRect;
+	cascade.detectMultiScale(frame_gray, detectedRect);
+	for (auto& det : detectedRect) {
+		Mat toSave = frame(det);
+		String toSavePath = "data/car_image_" + to_string(i++) + gen_random(5) + ".jpg";
+		imwrite(toSavePath, toSave);
+		cout << "Save: " << toSavePath << endl;
+	}
+}*/
+
 int main() {
-  const String cascade_name("cars.xml");
-  CascadeClassifier cascade;
-  
-  if (!cascade.load(cascade_name)) {
-    cout << "Error loading cars cascade\n";
-    return -1;
-  }
-  
-  VideoCapture capture;
-  capture.open("vid.mkv");
-  if (!capture.isOpened()) {
-    cout << "Error opening video capture";
-    return -1;
-  }
-  Mat frame, smol;
-  while (capture.read(frame)) {
-    if (frame.empty()) {
-      cout << "No more frames!\n";
-      break;
-    }
-    resize(frame, smol, Size(1240, 720));
-    detect(smol, cascade);
-    if (waitKey(10) == 27)
-      break;
-  }
-  return 0;
+	CascadeClassifier cascade;
+	VideoCapture capture;
+	Mat frame, smol;
+
+
+
+	capture.open("vid.mkv");
+	
+	if (!cascade.load("cars.xml") || !capture.isOpened()) return -1;
+
+	while (capture.read(frame)) {
+		if (frame.empty()) {
+			return -1;
+		}
+		resize(frame, smol, Size(1240, 720));
+		detect(smol, cascade);
+		if (waitKey(10) == 27)
+			break;
+	}
+	return 0;
 }
